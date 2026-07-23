8838cdee61f9f67527e2771a231e73bdabed941a
nhattranthinguyen <nhat.tranthinguyen@gmail.com>
2026-07-24 00:30:09 +0700

    refactor(test): let Mockito construct WalletService

--- src/test/java/com/nhattranthinguyen/wallet/wallet/application/WalletServiceTest.java
+++ src/test/java/com/nhattranthinguyen/wallet/wallet/application/WalletServiceTest.java
@@ -10,10 +10,10 @@ import static org.mockito.Mockito.when;
 import java.util.Optional;
 import java.util.UUID;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -28,12 +28,8 @@ class WalletServiceTest {
     @Mock
     private WalletRepository walletRepository;
 
+    @InjectMocks
     private WalletService walletService;
-
-    @BeforeEach
-    void setUp() {
-        walletService = new WalletService(walletRepository);
-    }
 
     @Test
     void shouldCreateWallet() {
