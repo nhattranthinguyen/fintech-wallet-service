42e110ea3ce1816c3bbb56270a8f16fc19d1c570
nhattranthinguyen <nhat.tranthinguyen@gmail.com>
2026-07-25 03:42:10 +0700

    fix(test): isolate PostgreSQL contexts between integration classes

--- src/test/java/com/nhattranthinguyen/wallet/support/PostgresIT.java
+++ src/test/java/com/nhattranthinguyen/wallet/support/PostgresIT.java
@@ -1,11 +1,13 @@
 package com.nhattranthinguyen.wallet.support;
 
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.annotation.DirtiesContext;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.postgresql.PostgreSQLContainer;
 
 @Testcontainers
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public abstract class PostgresIT {
     @SuppressWarnings("resource")
     @Container
